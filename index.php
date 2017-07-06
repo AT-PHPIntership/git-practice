0e2d3b0d3bde2cc5e9f68786462c13e0935f2b1b
Le Mai Vien <vien.le@asiantech.vn>
2017-07-06 13:26:52 +0700

    fix indent

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
 class CodingConvention
 {
-	private $openTime = '10:00';
+    private $openTime = '10:00';
 
-	private $closeTime = '22:00';
+    private $closeTime = '22:00';
 
-	private $isOpen = false;
+    private $isOpen = false;
 
-	public function startService(){
-		$this->isOpen = true;
-	}
+    public function startService(){
+        $this->isOpen = true;
+    }
 
 	public function stopService(){
-        $this->isOpen = false;
-    }
+		$this->isOpen = false;
+	}
 }
